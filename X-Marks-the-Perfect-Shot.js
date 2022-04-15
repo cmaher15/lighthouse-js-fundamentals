5e657464f4195de2b7e63b0e69aad890f524933a
cmaher15 <ceilidhemaher@hotmail.com>
2022-04-15 13:07:33 -0700

    The final changes to this code

--- X-Marks-the-Perfect-Shot.js
+++ X-Marks-the-Perfect-Shot.js
@@ -1,9 +1,9 @@
 const moves = ['north', 'south', 'west', 'east']
-const input = ['east', 'east', 'east', 'south']
+const input = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
 const finalPosition = function (moves) {
-  let x = 0
-  let y = 0
-for (let i = 0; i < moves.length; i++){
+  let x = 0;
+  let y = 0;
+for(let i = 0; i < moves.length; i++){
 if(moves[i] === 'north') {
   y++;
 } else if(moves[i] === 'south') {
@@ -11,11 +11,11 @@ if(moves[i] === 'north') {
 } else if (moves[i] === 'east') {
   x++;
 } else if (moves[i] === 'west') {
-  x--
+  x--;
+}
 }
 return [x,y]
-}
-} 
+}; 
 
 console.log(finalPosition(input));
 

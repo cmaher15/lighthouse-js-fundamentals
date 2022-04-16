a5a60fb665e35cb36377a4e5503b7e707619a939
cmaher15 <ceilidhemaher@hotmail.com>
2022-04-15 22:22:50 -0700

    Concantenate Arrays Task

---
+++ ConcatenateArrays.js
@@ -0,0 +1,11 @@
+let array1 = [];
+let array2 = [];
+const concat = function (array1, array2) {
+  let answer = array1.concat(array2)
+  return answer
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
